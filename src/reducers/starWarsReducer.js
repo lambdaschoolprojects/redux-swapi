b7f19fe74e82dac6e79d1689867dbc5699d25b86
RaneWallin <rane@ranewallin.com>
2019-04-16 15:51:54 -0700

    setting up action creator

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,7 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_SENT, FETCH_SUCCESS, FETCH_FAIL } from "../actions";
 const initialState = {
-  characters: []
+  characters: {},
+  isLoading: true,
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +9,13 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_SENT:
+      break;
+    case FETCH_SUCCESS:
+
+      break;
+    case FETCH_FAIL:
+      break;
     default:
       return state;
   }
