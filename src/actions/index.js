16cb4e14e6309a59f20fdffb6abe3e686200a470
RaneWallin <rane@ranewallin.com>
2019-04-16 15:38:46 -0700

    APPLYING MIDDLEWEAR

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
 // we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+const FETCH_SENT = "FETCH_SENT";
+const FETCH_SUCCESS = "FETCH_SUCCESS";
+const FETCH_FAIL = "FETCH_FAIL";
+
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
